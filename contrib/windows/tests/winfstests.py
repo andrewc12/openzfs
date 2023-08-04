127d00c8c09bf359ba6e1dc05525fdd5b534671c
Andrew Innes <andrew.c12@gmail.com>
2023-08-04 14:52:48 +0800

    Update winfstests.py

--- contrib/windows/tests/winfstests.py
+++ contrib/windows/tests/winfstests.py
@@ -239,7 +239,7 @@ def main():
                         total += 1
                         notok += 1
                 out = " ".join([str(test),
-                                ok, "/", total])
+                                str(ok), "/", str(total)])
 
                 print(out)
                 log_file.write(out)
